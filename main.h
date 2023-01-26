7c68f978f6e2c53e2b11ada992066ae7e8caacc8
maina-al3x <alexwachira4@gmail.com>
2023-01-26 07:10:53 -0800

    Task 0 I'm not going anywhere

--- main.h
+++ main.h
@@ -1,26 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
 int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 int _printf(const char *format, ...);
+void print_number(int n, int *count);
 
 /**
- * struct types - struct
- *
- * @c: data type
+ * struct vartype - struct vartype
+ * @vartype: type of variable to be printed
  * @f: the function associated
  */
 
-typedef struct types
+typedef struct vartype
 {
-  char c;
-  int (*f)(va_list);
-} types;
+	char *vartype;
+	int (*f)(va_list);
+} var_t;
 
+int c_func(va_list args);
+int s_func(va_list args);
+int perc_func(va_list args);
+int i_func(va_list args);
+int d_func(va_list args);
+int b_func(va_list args);
+int rev_func(va_list args);
+int rot_func(va_list args);
 
-
-#endif
+#endif /* MAIN_H */
